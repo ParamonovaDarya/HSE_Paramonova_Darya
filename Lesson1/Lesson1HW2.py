6aea9565a9c4c17c12c9e8e7dd5d3e0fcdb40851
ParamonovaDarya <150273364+ParamonovaDarya@users.noreply.github.com>
2024-02-09 15:12:00 +0300

    Add files via upload

---
+++ Lesson1/Lesson1HW2.py
@@ -0,0 +1,6 @@
+parties = [
+  {'name':str(input('Имя стороны: ')), 'status':str(input('Статус: ')), 'inn':str(input('ИНН: '))},
+  {'name':str(input('Имя стороны: ')), 'status':str(input('Статус: ')), 'inn':str(input('ИНН: '))},
+  {'name':str(input('Имя стороны: ')), 'status':str(input('Статус: ')), 'inn':str(input('ИНН: '))}
+]
+print(parties)
